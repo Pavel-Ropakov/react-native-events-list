3af559485d33b6a04ace08b60a2f452b22a5cf54
Pavel Rapakou <prapakou@klika-tech.com>
2018-04-13 09:38:26 +0300

    working infinite scroll added

--- eventList/EventList.js
+++ eventList/EventList.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import {Button, View, Text, StyleSheet, FlatList, Image, RefreshControl} from 'react-native';
-// import * as Animated from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  RefreshControl,
+  ActivityIndicator
+} from 'react-native';
+import styled from 'styled-components';
 
 export const colorPrimary = '#DC734A';
-const itemHeight = 80
+const itemHeight = 50
+const loader = <ActivityIndicator size="large" color={colorPrimary} />
 
 class EventList extends React.Component {
   constructor(props) {
@@ -13,19 +23,18 @@ class EventList extends React.Component {
       loading: false,
       refreshing: false,
       page: null,
-      // animatedScrollY: new Animated.Value(0),
     }
     this.fetchInitialData()
   }
 
-  static async fetchData(page = 0) {
-    const res = await fetch(`http://events-aggregator-workshop.anadea.co:8080/events?${page}`)
+  async fetchData (page=0) {
+    const res = await fetch(`http://events-aggregator-workshop.anadea.co:8080/events?page=${page}`)
     return res.json();
   }
 
-  fetchInitialData = async () => {
-    const responseJson = await EventList.fetchData()
-    this.setState({events: responseJson, refreshing: false, page: 0})
+  async fetchInitialData () {
+    const responseJson = await this.fetchData(0)
+    this.setState({events: responseJson, refreshing: false, page: 1})
   }
 
   onRefresh = async () => {
@@ -49,26 +58,33 @@ class EventList extends React.Component {
 
   onEndReached = async () => {
     const { loading, refreshing, page } = this.state;
-
     if (loading || refreshing || page === null) {
       return;
     }
-
     this.setState({ loading: true });
-
     const newEvents = await this.fetchData(page);
-
-    this.setState(({ events }) => ({
-      events: [...events, ...newEvents],
+    this.setState({
+      events: [...this.state.events, ...newEvents],
       loading: false,
       page: newEvents.length === 10 ? page + 1 : null,
-    }));
+    });
   };
 
-  // onScroll = Animated.event(
-  //   [{ nativeEvent: { contentOffset: { y: this.state.animatedScrollY } } }],
-  //   { useNativeDriver: true }
-  // );
+  onGetFooter() {
+    const { page } = this.state;
+
+    if (page === null) {
+      return null;
+    }
+
+    return loader
+  }
+
+
+  onGetItemLayout = (_, index) => {
+    return { length: itemHeight, offset: itemHeight * index, index };
+  };
+  keyExtractor = item => item._id;
 
   render() {
     const {events} = this.state
@@ -80,8 +96,13 @@ class EventList extends React.Component {
             // onScroll={this.onScroll}
             data={events}
             onEndReached={this.onEndReached}
-            onEndReachedThreshold={1}
-            animatedScrollY={this.state.animatedScrollY}
+            onEndReachedThreshold={0.5}
+            ListEmptyComponent={loader}
+            keyExtractor={this.keyExtractor}
+
+            getItemLayout={this.onGetItemLayout}
+            ListFooterComponent={this.onGetFooter.bind(this)}
+            // animatedScrollY={this.state.animatedScrollY}
             refreshControl={this.onGetRefreshControl()}
             renderItem={({item}) => (
               <View style={styles.item}>
@@ -99,7 +120,6 @@ class EventList extends React.Component {
               </View>
             )}
           />
-
         }
       </View>
     );
@@ -108,6 +128,23 @@ class EventList extends React.Component {
     title: 'Events',
   };
 }
+
+export const Bacground = styled.View`
+  background-color: 'white';
+  min-width: 100%;
+  min-height: 100%;
+`;
+
+// export const Scroll = styled(Animated.createAnimatedComponent(FlatList)).attrs({
+//   contentContainerStyle: {
+//     minWidth: '100%',
+//     minHeight: '100%',
+//   },
+// })`
+//   margin-top: ${Expo.Constants.statusBarHeight}px;
+// `;
+
+
 
 const styles = StyleSheet.create({
   container: {
